93ff24e57b9f92ce86bc1522c625a350d664bd28
david cuspoca <david-cuspoca@hotmail.com>
2022-04-15 18:31:54 -0600

    Modificamos la forma en que se hace login, ahora la respouesta la maneja desde el controller

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,7 @@
 const {Router} = require('express');
 const { check } = require('express-validator');
 const { renew, login } = require('../controllers/auth');
-const { loginExisteEmail } = require('../helpers');
+//const { loginExisteEmail } = require('../helpers');
 
 const { validarCampos, 
         validarJWT } = require('../middlewares')
@@ -10,7 +10,7 @@ const router = Router();
 
 router.post('/login', [
     check('email', 'El email es obligatorio').isEmail(),
-    check('email').custom(loginExisteEmail),
+    //check('email').custom(loginExisteEmail),
     check('password', 'El password es obligatorio').not().isEmpty(),
     validarCampos
 ], login)
